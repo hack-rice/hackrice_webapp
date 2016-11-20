71a876a07fdfdeb518f675717a301467ccd9e00d
jarrod <jarrodddunne@gmail.com>
2016-11-20 15:14:15 -0600

    Added js form filling based on JSON object

--- static/javascript/load_scoring_form.js
+++ static/javascript/load_scoring_form.js
@@ -1,3 +1,8 @@
 function loadScoringForm(serverJSON){
   document.getElementById("email").innerHTML = serverJSON["email"];
+  document.getElementById("university").innerHTML = serverJSON["university"];
 }
+
+window.onload = function(){
+  loadScoringForm({"email":"email","password":"pass","confirmed_password":"word","first_name":"first","last_name":"last","date_of_birth":"0001-01-01","github":"gh","personal_website":"my","linkedin":"li","university":"Rice University (TX)"});
+};
